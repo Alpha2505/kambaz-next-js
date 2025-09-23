59e7e46cd65607ad16ba1ebfa0b77948126ea200
Alpha2505 <gthnjlr@gmail.com>
2025-09-22 20:20:13 -0400

    fixed nav bug

--- app/(Kambas)/Courses/[cid]/Navigation.tsx
+++ app/(Kambas)/Courses/[cid]/Navigation.tsx
@@ -5,12 +5,12 @@ export default function CourseNavigation({ cid }: { cid: string }) {
     <div id="wd-courses-navigation">
       <Link href={`/Courses/${cid}/Home`} id="wd-course-home-link">Home</Link><br/>
       <Link href={`/Courses/${cid}/Modules`} id="wd-course-modules-link">Modules</Link><br/>
-      <Link href="" id="wd-course-piazza-link">Piazza</Link><br/>
-      <Link href="" id="wd-course-zoom-link">Zoom</Link><br/>
+      <Link href="/Courses/Piazza" id="wd-course-piazza-link">Piazza</Link><br/>
+      <Link href="/Courses/Zoom" id="wd-course-zoom-link">Zoom</Link><br/>
       <Link href={`/Courses/${cid}/Assignments`} id="wd-course-assignments-link">Assignments</Link><br/>
-      <Link href="" id="wd-course-quizzes-link">Quizzes</Link><br/>
-      <Link href="" id="wd-course-grades-link">Grades</Link><br/>
-      <Link href="" id="wd-course-people-link">People</Link><br/>
+      <Link href="/Courses/Quizzes" id="wd-course-quizzes-link">Quizzes</Link><br/>
+      <Link href="/Courses/Grades" id="wd-course-grades-link">Grades</Link><br/>
+      <Link href="/Courses/People" id="wd-course-people-link">People</Link><br/>
     </div>
   );
 }
