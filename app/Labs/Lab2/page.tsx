5f15f25176e1310009fddfca2d7cf72f54c95bc9
Alpha2505 <gthnjlr@gmail.com>
2025-09-22 21:30:27 -0400

    link to github

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -2,6 +2,7 @@ export default function Lab2() {
   return (
     <div>
       <h2>Lab 2</h2>
+      <h5>Link to my GitHub - </h5> <a href="https://github.com/Alpha2505/kambaz-next-js" target="_blank">https://github.com/Alpha2505/kambaz-next-js</a>
     </div>
 );}
 
