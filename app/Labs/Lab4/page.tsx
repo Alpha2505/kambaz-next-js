7c39fca7f28834b1bf7c7b6885c305784006d7a8
Geethanjali Ravichandra <geethanjaliravichandra@LOANERHXJMJ4QH265F.tld>
2025-10-31 19:02:50 -0700

    Starting with functions in Assignment 4

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariables from "./DateStateVariables";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div id="wd-lab4">
+      <h3>Lab 4 - Maintaining State in React Applications</h3>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariables />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
+    </div>
+  );
+}
