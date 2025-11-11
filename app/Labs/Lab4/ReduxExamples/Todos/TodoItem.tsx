793b5842d44d2dd66cbd38f014b3a3b5cbb0326e
Geethanjali Ravichandra <geethanjaliravichandra@LOANERHXJMJ5EV265F.tld>
2025-11-10 16:49:14 -0800

    bug fixes

--- app/Labs/Lab4/ReduxExamples/Todos/TodoItem.tsx
+++ app/Labs/Lab4/ReduxExamples/Todos/TodoItem.tsx
@@ -3,7 +3,11 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 import { ListGroupItem, Button } from "react-bootstrap";
 
-export default function TodoItem({ todo }) {
+export default function TodoItem({ 
+  todo 
+}: { 
+  todo: { id: string; title: string } 
+}) {
   const dispatch = useDispatch();
   
   return (
